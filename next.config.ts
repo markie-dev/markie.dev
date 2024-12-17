d2ed3bbf3078d493bc4553c10107fbd61e18ae31
marcus <markie@markie.dev>
2024-12-17 01:34:36 -0600

    update package-lock

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     formats: ['image/webp'],
   },
-  experimental: {
-    optimizeCss: true,
-  },
 };
 
 export default nextConfig;
